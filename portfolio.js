dc2c608899750198fe2dfc4fac810f6035af5233
Guled Ahmed <akindnesstoremember@gmail.com>
2021-01-05 23:34:31 -0800

    added a new song in the portfolio.html file

--- portfolio.js
+++ portfolio.js
@@ -2,12 +2,22 @@ $(function() {
 
     var audioElement = document.createElement("audio");
     audioElement.setAttribute("src", "assets/VanMorrisonPreciousTime.m4a");
+
+    var audioElement1 = document.createElement("audio");
+    audioElement1.setAttribute("src", "assets/BillyCurrington.m4a");
     
     $(".theme-button").on("click", function() {
       audioElement.play();
     });
     $(".pause-button").on("click", function() {
       audioElement.pause();
+    });
+
+    $(".theme-button1").on("click", function() {
+      audioElement1.play();
+    });
+    $(".pause-button1").on("click", function() {
+      audioElement1.pause();
     });
 
 });
